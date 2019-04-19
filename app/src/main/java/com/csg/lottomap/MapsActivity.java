10333b0d59d09eabfece924a1f7c44db5e675573
Choi sungkyu <maroon92@naver.com>
2019-04-19 19:47:45 +0900

    2. startLocationUpdates 로 callback 받아서 위치 갱신까지 함

--- app/src/main/java/com/csg/lottomap/MapsActivity.java
+++ app/src/main/java/com/csg/lottomap/MapsActivity.java
@@ -3,12 +3,22 @@ package com.csg.lottomap;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +30,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1000;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private LocationCallback mLocationCallback;
+    private LocationRequest locationRequest;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                // 진짜 위치 정보
+                for (Location location : locationResult.getLocations()) {
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15.0f));
+                    Toast.makeText(MapsActivity.this, "위치 갱신됨", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        };
+        // locationRequest 생성
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000);
+
+
     }
 
 
@@ -47,7 +89,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // 퍼미션 요청
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION
-                                ,Manifest.permission.ACCESS_COARSE_LOCATION},
+                                , Manifest.permission.ACCESS_COARSE_LOCATION},
                         MY_PERMISSIONS_REQUEST_FINE_LOCATION);
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
@@ -56,22 +98,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION
-                                ,Manifest.permission.ACCESS_COARSE_LOCATION},
+                                , Manifest.permission.ACCESS_COARSE_LOCATION},
                         MY_PERMISSIONS_REQUEST_FINE_LOCATION);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
-        }
-        // 권한 체크를 하고 켜줘야 함
+        } else {
+            // 권한승인이 된 경우
+            mMap.setMyLocationEnabled(true);
+
+            // 권한 체크를 하고 켜줘야 함
 //        mMap.setMyLocationEnabled(true);
+
+        }
 
         // 승인
         // 초기화 위치 위경도
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10.0f));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10.0f));
 
 
     }
@@ -89,24 +136,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // 허락 됏으면 켜주기
-                   mMap.setMyLocationEnabled(true);
+                    mMap.setMyLocationEnabled(true);
+                    startLocationUpdates();
 
 
-                   // permission was granted, yay! Do the
+                    // permission was granted, yay! Do the
                     // contacts-related task you need to do.
                 } else {
                     // permission denied,
-                   // 거부했을때 , 재요청하는 다이얼로그 띄우기 try again to request the permission.
-                   AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                   builder.setTitle("권한")
-                           .setMessage("설정에서 언제든지 설정할 수 있음")
-                           .setPositiveButton("설정", new DialogInterface.OnClickListener() {
-                               public void onClick(DialogInterface dialog, int id) {
-                                   // Send the positive button event back to the host activity
-                               }
-                           })
-                           .setNegativeButton("닫기", null)
-                           .show();
+                    // 거부했을때 , 재요청하는 다이얼로그 띄우기 try again to request the permission.
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("권한")
+                            .setMessage("설정에서 언제든지 설정할 수 있음")
+                            .setPositiveButton("설정", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // 설정 화면
+                                    Intent intent = new Intent();
+                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                    intent.setData(uri);
+                                    startActivity(intent);
+                                }
+                            })
+                            .setNegativeButton("닫기", null)
+                            .show();
 
                 }
                 return;
@@ -115,5 +168,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // other 'case' lines to check for other
             // permissions this app might request.
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // 지속적으로 현재위치를 얻게 하는 코드
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+
+        // 거부
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        } else {
+
+            // 허락
+            mFusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                    mLocationCallback,
+                    null /* Looper */);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
     }
 }
